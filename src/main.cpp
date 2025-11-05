66bfef353649db17699cb53e30c7811c9d38ad18
TheLastGimbus <mateusz.soszynski@tuta.io>
2025-11-05 09:43:13 +0100

    JA PD TO DZIAŁA

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <Arduino.h>
 void setup() {
+    pinMode(LED_BUILTIN, OUTPUT);
 // write your initialization code here
 }
 
 void loop() {
+    digitalWrite(LED_BUILTIN, 1);
+    delay(100);
+    digitalWrite(LED_BUILTIN, 0);
+    delay(100);
 // write your code here
 }
