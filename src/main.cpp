2f203de1b913e0ae9c327f02198426fe90418b9b
TheLastGimbus <mateusz.soszynski@tuta.io>
2025-11-16 23:18:11 +0100

    MADE TMP WORK!!

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,26 @@
-#include <Arduino.h>
+#include <Wire.h>
+#include <TMP117.h>
+
+uint8_t ADDR_GND = 0x48; // 1001000
+uint8_t ADDR_VCC = 0x49; // 1001001
+uint8_t ADDR_SDA = 0x4A; // 1001010
+uint8_t ADDR_SCL = 0x4B; // 1001011
+uint8_t ADDR = ADDR_GND;
+TMP117 tmp(ADDR);
+
+void newTempEmpty() {
+}
+
 void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
-// write your initialization code here
+    Serial.begin(9600);
+    Wire.begin();
+
+    tmp.init(newTempEmpty);
 }
 
 void loop() {
-    digitalWrite(LED_BUILTIN, 1);
-    delay(100);
-    digitalWrite(LED_BUILTIN, 0);
-    delay(100);
-// write your code here
+    Serial.print(tmp.getTemperature());
+    Serial.println(" °C");
+    // WORKS !!!
+    delay(500);
 }
